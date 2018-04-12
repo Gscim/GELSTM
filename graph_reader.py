bdad5ff430af14583191407c8b8e22937e60ae55
Gscim <gscim.yn@gmail.com>
2018-04-12 16:11:50 +0800

    v1_1

--- graph_reader.py
+++ graph_reader.py
@@ -18,8 +18,18 @@ def _read_ent_rel(filename):
         return f.read().strip().split()
 
 def _build_vocab(filename):
+    '''
+    sorted by frequency of words(here words means nodes and relations)
+    '''
     data = _read_ent_rel(filename)
     counter = collections.Counter(data)
     count_pairs = sorted(couter.items(), key=lambda x:(-x[1], x[0]))
 
+    word, _ = list(zip(*count_pairs))
+    word2id = dict(zip(words, range(len(words))))
     
+    return word2id
+
+
+
+
